135a4ce2299b422e2118a3cc482edab03a6bb071
Barun Singh <bsingh@wegowise.com>
2011-05-22 12:27:08 -0400

    minor cleanup

--- lib/memoizer.rb
+++ lib/memoizer.rb
@@ -8,21 +8,21 @@ module Memoizer
     method_name.to_s.sub(/\?\Z/, '_query').sub(/!\Z/, '_bang').to_sym
   end
 
-  module ClassMethods
-    attr_accessor :_memoizer_methods
+  def self.ivar_name(method_name)
+    "_memoized_#{self.safe_name(method_name)}"
+  end
 
+  module ClassMethods
     def memoize(*method_names)
       method_names.each do |method_name|
         safe_method_name = Memoizer.safe_name(method_name)
+        memoized_ivar_name = Memoizer.ivar_name(method_name)
+        unmemoized_method = "_unmemoized_#{method_name}"
 
-        memoized_ivar_name = "_memoized_#{safe_method_name}"
-        aliased_original_method_name = "_unmemoized_#{method_name}"
         attr_accessor memoized_ivar_name
-        alias_method aliased_original_method_name, method_name
+        alias_method unmemoized_method, method_name
 
-        no_args = self.instance_method(aliased_original_method_name).arity == 0
-        is_private = self.private_method_defined?(aliased_original_method_name)
-        is_protected = self.protected_method_defined?(aliased_original_method_name)
+        no_args = self.instance_method(unmemoized_method).arity == 0
 
         define_method method_name do |*args|
           memoized_value = self.instance_variable_get("@#{memoized_ivar_name}")
@@ -30,7 +30,7 @@ module Memoizer
           # if the method takes no inputs, store the value in an array
           if no_args
             if !memoized_value.is_a?(Array)
-              memoized_value = [self.send(aliased_original_method_name)]
+              memoized_value = [self.send(unmemoized_method)]
               self.instance_variable_set("@#{memoized_ivar_name}", memoized_value)
             end
             memoized_value.first
@@ -38,18 +38,18 @@ module Memoizer
           #otherwise store in a hash indexed by the arguments
           else
             if !memoized_value.is_a?(Hash)
-              memoized_value = {args => self.send(aliased_original_method_name, *args)}
+              memoized_value = {args => self.send(unmemoized_method, *args)}
               self.instance_variable_set("@#{memoized_ivar_name}", memoized_value)
             elsif !memoized_value.has_key?(args)
-              memoized_value[args] = self.send(aliased_original_method_name, *args)
+              memoized_value[args] = self.send(unmemoized_method, *args)
             end
             memoized_value[args]
           end
         end
 
-        if is_private
+        if self.private_method_defined?(unmemoized_method)
           private method_name
-        elsif is_protected
+        elsif self.protected_method_defined?(unmemoized_method)
           protected method_name
         end
 
@@ -59,7 +59,7 @@ module Memoizer
 
   module InstanceMethods
     def unmemoize(method_name)
-      self.instance_variable_set("@_memoized_#{Memoizer.safe_name(method_name)}", nil)
+      self.instance_variable_set("@#{Memoizer.ivar_name(method_name)}", nil)
     end
 
     def unmemoize_all
